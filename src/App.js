7bfcc77c22e6b3c9164c171787d050f7d685e2c0
= <codenation@Codes-MacBook-Pro.local>
2020-05-10 14:17:36 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -2,25 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+  const APP_ID = '2a538a1a';
+  const APP_KEY = '6988e3cd04946467a744df460214c895';
+
+  const examplereq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>hello</h1>
     </div>
   );
-}
+};
 
 export default App;
