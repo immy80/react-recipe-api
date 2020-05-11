86fff07e91adca4a9de0aae14a00a534a52ae086
= <codenation@Codes-MacBook-Pro.local>
2020-05-11 13:43:35 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import Recipe from './components/recipe';
 import './App.css';
 
 const App = () => {
   const APP_ID = '2a538a1a';
   const APP_KEY = '6988e3cd04946467a744df460214c895';
 
-  const examplereq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState('');
+  const [query, setQuery] = useState('');
+
+  useEffect(() => {
+    getRecipe();
+  }, [query]);
+  const getRecipe = async () => {
+    const response = await fetch(
+      `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
+    );
+    const data = await response.json();
+    setRecipes(data.hits);
+  };
+
+  const updateSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const getSearch = (e) => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch('');
+  };
+
   return (
     <div className='App'>
-      <h1>hello</h1>
+      <h1>Recipe Finder</h1>
+      <form onSubmit={getSearch} className='search-form'>
+        <input
+          className='search-bar'
+          type='text'
+          value={search}
+          onChange={updateSearch}
+        />
+        <button className='search-button' type='submit'>
+          Search
+        </button>
+      </form>
+      {recipes.map((recipe) => (
+        <Recipe
+          key={recipe.recipe.label}
+          title={recipe.recipe.label}
+          calories={recipe.recipe.calories}
+          image={recipe.recipe.image}
+        />
+      ))}
     </div>
   );
 };
